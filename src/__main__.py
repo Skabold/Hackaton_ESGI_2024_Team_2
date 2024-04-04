7bfc85f4573de1571adc63055b8d4f3c0d4b8d31
Linz Pierre <pierre.linz@orange.fr>
2024-04-04 18:58:19 +0200

    whatever

--- src/__main__.py
+++ src/__main__.py
@@ -9,20 +9,7 @@ from data_processing.data_processing_function import calculate_delay_rate
 
 
 def main():
-    # Sample data for testing
-    interval = 30 * 3600 * 24
-    delayed_date = 1712217600
-    current_date = 1712244726
-    num_trips = 5
-
-    # Call the function
-    try:
-        delay_rate = calculate_delay_rate(
-            interval, delayed_date, current_date, num_trips
-        )
-        print("Delay Rate:", delay_rate)
-    except RuntimeError as e:
-        print("Error occurred:", e)
+    print("hello world")
 
 
 if __name__ == "__main__":
