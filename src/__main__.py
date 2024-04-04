1a3fad4661e88c84c0cd7d1b6e886e944d79a074
Linz Pierre <pierre.linz@orange.fr>
2024-04-04 19:40:00 +0200

    fix

--- src/__main__.py
+++ src/__main__.py
@@ -5,11 +5,15 @@ This script serves as the entry point for the project.
 It reads input data, performs data processing, and potentially outputs results.
 """
 
-from data_processing.data_processing_function import calculate_delay_rate
+from data_processing.data_processing_function import train_delay_rate
 
 
 def main():
-    print("hello world")
+    # Call the function
+    delay_rate = train_delay_rate("data/input/raw.csv")
+
+    # Print the result or do whatever you want with it
+    print("Average delay rate:", delay_rate)
 
 
 if __name__ == "__main__":
